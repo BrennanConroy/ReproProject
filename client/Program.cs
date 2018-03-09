8dcf8e64498bb1cb842c9c3f0db5d0ff9f2130dd
Brennan <brennan@Brennans-MacBook-Air.local>
2018-03-09 11:51:47 -0800

    repo projects

---
+++ client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace tcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:5000");
+            request.Content = new StringContent("test");
+            var response = client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
